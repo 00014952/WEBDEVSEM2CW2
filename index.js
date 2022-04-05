7c831115b9b45df940e8273bfe6a837c4567c90c
wiut00014952@gmail.com <wiut00014952@gmail.com>
2022-04-05 18:19:58 +0500

    more commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+const Job = require("./job");
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.engine("ejs", ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+mongoose.connect(
+  "mongodb+srv://user:1234user5678@cluster0.uqvt1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("DB IS CONNECTED");
+});
 
 app.get("/", (req, res) => res.render("home"));
+
+app.get("jobs", (req, res) => {
+  const jobs = Job.find();
+  res.render("jobs", { jobs });
+});
 
 app.get("todos", (req, res) => {
   fs.readFile(DB, (err, data) => {
