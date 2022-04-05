2ebdfdec509eeb6c9dace016415214dffe7425b0
wiut00014952@gmail.com <wiut00014952@gmail.com>
2022-04-05 20:43:31 +0500

    last commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const Job = require("./job");
 const methodOverride = require("method-override");
+const job = require("./job");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -19,34 +20,59 @@ db.on("error", console.log.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("DB IS CONNECTED");
 });
-
+//home page
 app.get("/", (req, res) => res.render("home"));
-
+//get list of jobs
 app.get("/jobs", async (req, res) => {
   const jobs = await Job.find();
   res.render("jobs", { jobs });
 });
+//create form
 app.get("/jobs/create", (req, res) => res.render("create"));
-app.post("/jobs", async (req, res) => {
-  const job = new Job(req.body.job);
-  await job.save();
-  res.redirect("/jobs");
+//create action
+app.post("/jobs", async (req, res, next) => {
+  try {
+    const job = new Job(req.body.job);
+    await job.save();
+    res.redirect("/jobs");
+  } catch (error) {
+    next(error);
+  }
 });
-
+//details
 app.get("/jobs/:id", async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
   res.render("details", { job });
 });
-app.get("/jobs/:id/update", async (req, res) => {
-  const { id } = req.params;
-  const job = await Job.findById(id);
-  res.render("update", { job });
+//update form
+app.get("/jobs/:id/update", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const job = await Job.findById(id);
+    res.render("update", { job });
+  } catch (error) {
+    next(error);
+  }
 });
+//update
 app.put("/jobs/:id", async (req, res) => {
   const { id } = req.params;
   const job = await Job.findByIdAndUpdate(id, req.body.job);
   res.redirect("/jobs");
 });
-
+//remove
+app.delete("/jobs/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Job.findByIdAndDelete(id);
+    res.redirect("/jobs");
+  } catch (error) {
+    next(error);
+  }
+});
+//errror handling
+app.use((err, req, res, next) => {
+  res.render("error", { err });
+});
 app.listen(3000, () => console.log("Server started on port 3000"));
